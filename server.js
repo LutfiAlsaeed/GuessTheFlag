b637c51a34e315c0de1a1891ea39cbb906ee9597
LutfiAlsaeed <lutfinader@gmail.com>
2022-10-10 19:32:36 +0300

    working on api

--- server.js
+++ server.js
@@ -2,10 +2,34 @@ const express=require('express');
 const app=express();
 const PORT=5000;
 
+const country={
+  'jo':{
+    'nameEn':'Jordan',
+    'nameAr':'الاردن',
+    'info':'',
+    'flag':''
+  },
+  'af':{
+    'nameEn':'Afghanistan',
+    'nameAr':'أفغانستان',
+    'info':'',
+    'flag':'https://www.worldometers.info/img/flags/af-flag.gif'
+  }
+  
+}
+
 app.get('/',(req,res)=>{
   res.sendFile(__dirname+'/homePage/homePage.html')
 })
 
+app.get('/api/:flag',(req,res)=>{
+  const reqCountry=req.params.flag.toLowerCase();
+  if(country[reqCountry]){
+    res.json(country[reqCountry])
+  }
+  res.json(country)
+})
+
 app.listen(PORT,()=>{
-  console.log('here we goooo');
+  console.log(`here we goooo on port${PORT}`);
 })
